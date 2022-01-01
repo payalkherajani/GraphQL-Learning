fd8a9388802a24edd850907c85971c9f283cf630
payalkherajani <payal.kherajani19@gmail.com>
2022-01-01 21:50:37 +0530

    schema-modified

--- schema.js
+++ schema.js
@@ -1,5 +1,59 @@
 const graphql = require('graphql')
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = graphql
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList } = graphql
+
+const posts = [
+    {
+        title: 'First post',
+        description: 'Content of the first post',
+        author: 'Flavio',
+    },
+    {
+        title: 'Second post',
+        description: 'Content of the second post',
+        author: 'Roger',
+    },
+]
+
+const authors = {
+    Flavio: {
+        name: 'Flavio',
+        age: 36,
+    },
+    Roger: {
+        name: 'Roger',
+        age: 7,
+    },
+}
+
+const authorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: {
+        name: {
+            type: GraphQLString
+        },
+        age: {
+            type: GraphQLInt
+        }
+    }
+})
+
+const postType = new GraphQLObjectType({
+    name: 'Post',
+    fields: {
+        title: {
+            type: GraphQLString
+        },
+        description: {
+            type: GraphQLString
+        },
+        author: {
+            type: authorType,
+            resolve: (source, params) => {
+                return authors[source.author]
+            }
+        }
+    }
+})
 
 const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
@@ -11,9 +65,25 @@ const schema = new GraphQLSchema({
                     return "world"
                 }
 
+            },
+            post: {
+                type: postType,
+                args: {
+                    id: { type: GraphQLInt }
+                },
+                resolve: (source, { id }) => {
+                    return posts[id]
+                }
+            },
+            posts: {
+                type: new GraphQLList(postType),
+                resolve: () => {
+                    return posts
+                }
             }
         }
     })
 })
+
 
 module.exports = schema
